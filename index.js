3a357ad62dc133b6fa0ad97513dfe0ffe5604b93
Mahesh Kumar <ml4909854@gmail.com>
2024-02-03 12:00:49 +0530

    prime code

---
+++ index.js
@@ -0,0 +1,15 @@
+
+
+let count = 0
+let num = 13;
+for(let i=1; i<=num; i++){
+    if(num%i!==0){
+        count++
+    }
+}
+if(count==2){
+    console.log("yes")
+}
+else{
+    console.log("No")
+}
